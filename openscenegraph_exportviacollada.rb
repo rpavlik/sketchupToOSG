d590cdfb6ba9ee2e168dbcbb07f16f4604e36282
Ryan Pavlik <rpavlik@iastate.edu>
2013-09-19 15:43:24 -0500

    Update openscenegraph_exportviacollada.rb
    Bump version

--- openscenegraph_exportviacollada.rb
+++ openscenegraph_exportviacollada.rb
@@ -10,7 +10,7 @@ require 'extensions.rb'
 
 module RP_SketchUpToOSG
     osg_exportviacollada_extension = SketchupExtension.new "Export to OpenSceneGraph (via COLLADA)", "osgconv/osgconv.rb"
-    osg_exportviacollada_extension.version = '1.6.1'
+    osg_exportviacollada_extension.version = '1.6.2'
     osg_exportviacollada_extension.description = "Export to OpenSceneGraph formats, by first exporting to COLLADA then converting. Accessible via a submenu of File, unfortunately not in Export due to SketchUp API limitations. Homepage: https://github.com/rpavlik/sketchupToOSG"
     osg_exportviacollada_extension.copyright = "2011, 2013, Iowa State University VRAC"
     osg_exportviacollada_extension.creator = "Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>"
