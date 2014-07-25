0189bbd84a332c2233b2b8b3454757d074a0b40f
Ryan Pavlik <rpavlik@iastate.edu>
2014-07-25 16:43:33 -0500

    Remove old script

--- install_and_run.cmd
+++
@@ -1,6 +0,0 @@
-cd %~dp0
-set SKETCHUPDIR=%ProgramFiles(x86)%\Google\Google SketchUp 8
-copy openscenegraph_exportviacollada.rb "%SKETCHUPDIR%\Plugins"
-mkdir "%SKETCHUPDIR%\Plugins\osgconv"
-copy osgconv\*.rb "%SKETCHUPDIR%\Plugins\osgconv"
-"%SKETCHUPDIR%\SketchUp.exe"
